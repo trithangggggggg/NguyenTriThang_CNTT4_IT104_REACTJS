dc8acbbd1d4fc9b7d13e1426b3d1293ab0199916
trithangggggggg <trithang2006vn@gmail.com>
2025-08-07 03:10:30 +0700

    moi

---
+++ PTIT_CNTT4_IT104_Session3/PTIT_CNTT4_IT104_Session03_Bai02/index.ts
@@ -0,0 +1,4 @@
+// let userName : string ;
+// let userName : number ;
+
+// loi vi khong the gan 1 bien voi 2 kieu du lieu khac nhau 
---
+++ PTIT_CNTT4_IT104_Session3/PTIT_CNTT4_IT104_Session03_Bai03/index.ts
@@ -0,0 +1,11 @@
+let isGreeting :boolean = true;
+if(isGreeting){
+    console.log("Xin chao ");
+}else{
+    console.log("Tam biet ");
+    
+}
+
+
+
+
